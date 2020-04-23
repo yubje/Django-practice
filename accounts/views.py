db063e27a0719fdb04363945730d66b0324db3d0
제유빈 <yubinje630@gmail.com>
2020-04-23 05:55:22 +0000

    accounts | login & logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login, logout as auth_logout
+from django.contrib.auth.decorators import login_required
 
 def signup(request):
+    if request.user.is_authenticated:
+        return redirect('community:review_list')
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -13,3 +17,23 @@ def signup(request):
         'form': form
     }
     return render(request, 'accounts/signup.html', context)
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('community:review:list')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('community:review_list')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/login.html', context)
+
+@login_required
+def logout(request):
+    auth_logout(request)
+    return redirect('community:review_list')
